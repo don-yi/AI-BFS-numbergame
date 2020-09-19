0e14d60abff4792465d3f09c2544f3f6068ef117
Dennis-Donguk <dennis.donguk.lee@gmail.com>
2020-09-18 21:16:54 -0700

    progress up for jb

--- numgam/source/numbergame.cpp
+++ numgam/source/numbergame.cpp
@@ -1,8 +1,159 @@
 #include <tuple>
 #include "numbergame.h"
 
-std::vector<std::tuple<unsigned, unsigned>> SolveNumbergameBFS(std::string const& filename)
+std::vector<std::tuple<unsigned, unsigned>>
+SolveNumbergameBFS(std::string const& filename)
 {
-  std::vector<std::tuple<unsigned, unsigned>> res;
-  return res;
+  // GameField implementation
+  unsigned            w= 0;
+  std::vector< int >  field = std::vector< int >();    // flat array
+
+  std::ifstream infile(filename);
+  if (not infile.good()) {
+    throw "Cannot open input file";
+  }
+
+  // read width
+  infile >> w;
+
+  int input;
+  infile >> input;
+  while (infile.good() and not infile.eof()) {
+    field.push_back(input);
+    infile >> input;
+  }
+
+
+  // main bfs impl
+  std::vector<std::tuple<unsigned, unsigned>> sol;
+
+  do
+  {
+    // q init
+    std::queue<int> q;
+    q.push(0);
+    while (not q.empty())
+    {
+      // get num ind from q
+      unsigned fr = q.front();
+      unsigned num = field[fr];
+
+      // handle q
+      q.pop();
+      if ((fr + 1) % w and (fr + 1) < field.size())
+      {
+        q.push(fr + 1);
+      }
+      if ((fr + w) < field.size())
+      {
+        q.push(fr + w);
+      }
+
+      // handle game logic
+      if (not num)
+      {
+        continue;
+      }
+
+      std::tuple<unsigned, unsigned> pair = RmPair(field, fr, w);
+      if (not std::get<1>(pair))
+      {
+        continue;
+      }
+      sol.push_back(pair);
+    }
+  } while (IsChecking(field)); // til solved (no checks)
+
+  return sol;
 }
+
+std::tuple<unsigned, unsigned>
+RmPair(std::vector<int>& field, unsigned ind, unsigned width)
+{
+  // search towards right
+  for (size_t i = ind + 1; i < field.size(); i++)
+  {
+    if (field[i] == 0)
+    {
+      continue;
+    }
+
+    if (field[i] == field[ind])
+    {
+      field[i] = 0;
+      field[ind] = 0;
+      return std::make_tuple(ind, i);
+    }
+    else
+    {
+      break;
+    }
+  }
+
+    // search towards left
+  // zero sub check
+  if (ind - 1 >= 0)
+  {
+    for (int i = ind - 1; i >= 0; --i)
+    {
+      if (field[i] == 0)
+      {
+        continue;
+      }
+
+      if (field[i] == field[ind])
+      {
+        field[i] = 0;
+        field[ind] = 0;
+        return std::make_tuple(ind, i);
+      }
+      else
+      {
+        break;
+      }
+    }
+  }
+
+  // num below
+  unsigned downInd = ind + width;
+  if (downInd < field.size() and field[downInd] == field[ind])
+  {
+    field[downInd] = 0;
+    field[ind] = 0;
+    return std::make_tuple(ind, downInd);
+  }
+
+  // num above
+  int topInd = ind - width;
+  if (topInd >= 0 and field[topInd] == field[ind])
+  {
+    field[topInd] = 0;
+    field[ind] = 0;
+    return std::make_tuple(ind, topInd);
+  }
+
+  return std::make_tuple(ind, 0);
+}
+
+bool IsChecking(std::vector<int>& field)
+{
+  // init checking bool
+  bool isChecking = false;
+
+  // find existing num
+  unsigned size = field.size();
+  for (size_t i = 0; i < size; i++)
+  {
+    if (not field[i])
+    {
+      continue;
+    }
+
+    // do check
+    field.push_back(field[i]);
+    isChecking = true;
+  }
+
+  return isChecking;
+}
+
